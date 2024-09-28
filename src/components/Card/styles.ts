447ee2df55aa50db4636d09dbbe2891b17df64a5
Daniel Barreto <dbarretobrito@gmail.com>
2024-09-27 21:17:54 -0300

    teste z 2

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -34,3 +34,64 @@ export const CardContainer = styled.div`
     max-width: 13rem;
   }
 `;
+
+
+export const CardImage = styled.img`
+  width: 100%; /* A largura da imagem será sempre 100% */
+  height: auto; /* Altura automática para manter a proporção */
+  max-height: 85%; /* Limita a altura máxima da imagem */
+  object-fit: cover; /* Garante que a imagem preencha o espaço mantendo a proporção */
+
+  /* Para garantir a proporção do contêiner */
+  @supports not (-webkit-touch-callout: none) {
+    aspect-ratio: inherit; /* Mantém a proporção do contêiner */
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    max-height: 80%; /* Ajusta ligeiramente a altura para tablets */
+  }
+`;
+
+export const CardContent = styled.div`
+  font-family: 'Poppins', sans-serif;
+  padding: 10px 0;
+  text-align: center;
+  height: auto; /* Ajuste automático da altura */
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  p {
+    margin: 0;
+    color: white;
+  }
+
+  span {
+    font-size: 0.85rem;
+    color: #888;
+    margin-top: 3px;
+  }
+
+  @media (max-width: 665px) {
+    font-size: 0.8rem;
+    span {
+      font-size: 0.7rem;
+    }
+  }
+
+  @media (max-width: 515px) {
+    font-size: 0.75rem;
+    span {
+      font-size: 0.65rem;
+    }
+  }
+
+  @media (max-width: 480px) {
+    font-size: 0.5rem;
+    margin-top: 5px;
+    span {
+      font-size: 0.6rem;
+    }
+  }
+`;
