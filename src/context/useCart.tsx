cadb457753eb1df779520d8d50caf52f1977c9e3
Daniel Barreto <dbarretobrito@gmail.com>
2024-11-11 10:03:08 -0300

    Cria hook useCart para acessar o contexto do carrinho

--- src/context/useCart.tsx
+++ src/context/useCart.tsx
@@ -1,10 +1,17 @@
+// Importa o hook useContext do React e os tipos e contexto CartContext definidos em CartContext.tsx
 import { useContext } from 'react';
 import { CartContext, CartContextType } from './CartContext';
 
+// Define um hook personalizado chamado useCart que retorna o valor do contexto do carrinho
 export const useCart = (): CartContextType => {
+  // Usa o hook useContext para acessar o CartContext
   const context = useContext(CartContext);
+
+  // Verifica se o contexto está indefinido, o que significa que useCart foi usado fora de um CartProvider
   if (!context) {
     throw new Error('useCart deve ser usado dentro de um CartProvider');
   }
+
+  // Retorna o contexto, fornecendo acesso às funções e ao estado do carrinho
   return context;
 };
