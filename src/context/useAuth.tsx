400d89b24156874eabe49b346c5961493b43eb44
Daniel Barreto <dbarretobrito@gmail.com>
2024-11-11 10:30:54 -0300

    Adicionar hook useAuth para acesso ao contexto de autenticação

--- src/context/useAuth.tsx
+++ src/context/useAuth.tsx
@@ -1,10 +1,14 @@
 import { useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
+// Hook personalizado para acessar o contexto de autenticação
 export const useAuth = () => {
+  // Obtém o contexto de autenticação usando useContext
   const context = useContext(AuthContext);
+  // Garante que o hook seja utilizado dentro do provedor adequado (AuthProvider)
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
+  // Retorna o valor do contexto para ser usado nos componentes que chamarem este hook
   return context;
 };
